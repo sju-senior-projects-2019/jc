f292ba506e06628542fcefc026a7c0e77edae146
Jackary <JackColemanCS@gmail.com>
2019-04-16 21:58:58 -0400

    Added predicate generator for define-type

--- hawk/expander.rkt
+++ hawk/expander.rkt
@@ -1,17 +1,19 @@
-#lang racket/base
+#lang racket
 
 
 (require racket (for-syntax racket/syntax syntax/parse))
 (provide (except-out (all-from-out racket) define)
          (rename-out [define~ define])
+         define-type
          Real)
 
-
+;Test macro 
 (define-syntax Real
     (lambda (stx)
       (syntax-case stx ()
         [Real (identifier? #'Real) #'real?])))
 
+;Implements dynamic typechecking
 (define-syntax (define~ stx)
   (syntax-parse stx
     [(_ name:id type:expr x:expr)
@@ -23,6 +25,22 @@
      #'(define/contract
          (name args ...)
          typedef
-         body)]
-    ))
-         
+         body)]))
+
+(define-syntax (define-type stx)
+  (syntax-parse stx
+    [(_ name:id (pred ...))
+     (with-syntax
+        ([pred-name
+          (datum->syntax
+           #'name
+           (string->symbol
+            (string-downcase
+             (format "~a?" (syntax->datum #'name)))))])
+     #''(begin
+         (define (pred-name arg)
+           (and (pred arg) ...))))]))
+
+
+
+(define-type NUM (number? real?))
